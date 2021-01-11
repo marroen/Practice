3cc7d4fc5ae3ad27892fa1f486361c17999e3947
martyro <47480540+martyro@users.noreply.github.com>
2021-01-11 14:28:16 +0100

    new test

--- test.rb
+++ test.rb
@@ -1,29 +1,5 @@
-class BinarySearch
-
-	def get_limits(array)
-		[0, array.length - 1]
-	end
-		
-	def binary_search_recursive(array, key)
-	
-		low, high = get_limits(array)
-		
-		mid = (low + high) / 2
-		
-		return -1 if low > high
-		
-		return binary_search_recursive(array[low, mid-1], key) if (array[mid] > key)
-		
-		return binary_search_recursive(array[mid+1, high], key) if (array[mid] < key)
-		
-		return mid if (array[mid] == key)
-		
-	end
-		
-end		
-
-arr = [1,3,4,12,16,21,34,45,55,76,99,101]
-key = 21
-s = BinarySearch.new
-index = s.binary_search_recursive(arr, key)
-puts index
+class Test
+  def test
+    p 'sup'
+  end
+end
